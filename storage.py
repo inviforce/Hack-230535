28125e61e2b4eefebc43a50ebdf5f4813c1b01e4
Deepak_2023 <saraswatdeepak0212@gmail.com>
2023-10-10 18:48:33 +0530

    Commit01

---
+++ storage.py
@@ -0,0 +1,4 @@
+username="Deepak"
+Location="chennai"
+max_temp=24
+min_temp=23
--- temperature.py
+++ temperature.py
@@ -1,34 +1,37 @@
 import requests
-from uagents import Agent, Context, Bureau, Model
+import os
+import storage
+from colorama import Fore
+from dotenv import load_dotenv
+from uagents import Agent, Context
 from uagents.setup import fund_agent_if_low
-#first step is to get information from the client
-city_name=input("Enter city name: ")
-min_temp=int(input("Enter the minimum temperature in degree Celsius: "))
-max_temp=int(input("Enter the maximum temperature in degree Celsius: "))
-#second step is to acess information from the api
-weather_api_key='b522b5ef009540ff2b942076593fb43f'
+#-------FIRST STEP IS TO GET INFORMATION FROM CLIENT----------
+city_name=storage.Location
+min_temp=float(storage.min_temp)
+max_temp=float(storage.max_temp)
+#-------SECOND STEP IS TO GET INFORMATION FROM OPEN WEATHER API--------
+load_dotenv()
+weather_api_key=os.getenv('weather_api_key')
 weather_api=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={weather_api_key}'
 params={"units":"metric"}
-response_api=requests.get(weather_api,params=params)
+#-------------CHECKING FOR THE CORRECT TEMPERATURE RANGE------------
 async def alert_temperature(ctx:Context,alert):
     try:
-        # Access the content of the response and check for the condition of temperature
+        response_api=requests.get(weather_api,params=params)
         content = response_api.json()
-        if content["main"]["temp"]>max_temp:
-            ctx.logger.info(f"Alert:Temperature in city {city_name} is exceeding maximum temperature, Current temperature is {content['main']['temp']}")
-        elif content["main"]["temp"]<min_temp:
-            ctx.logger.info(f"Alert:Temperature in city {city_name} is below minimum temperature, Current temperature is {content['main']['temp']}")
-    except Exception as e:
-        ctx.logger.info(f"Error fetching data for{city_name},check the spelling or the given city doesn't exist in our data base")
-#to create an agent
-temp_alert=Agent(name="Alert",seed="Alert recovery phase",endpoint=['https://api.openweathermap.org'])
+        if content["main"]["temp"]>alert['max_temperature']: #------CHECKING FOR MAXIMUM TEMPERATURE---------
+            ctx.logger.info(Fore.RED+f"Alert: Temperature in {alert['location']} is above {alert['max_temperature']}")
+        elif content["main"]["temp"]<alert['min_temperature']:#-------CHECKING FOR MINIMUM TEMPERATURE--------
+            ctx.logger.info(Fore.RED+f"Alert: Temperature in  {alert['location']} is below {alert['min_temperature']}")
+    except Exception:#--------IN CASE OF SOME ERROR IN EXTRACTING INFORMATION----------
+        ctx.logger.info(Fore.MAGENTA+f"Error fetching data for {alert['location']},check the spelling or the given city doesn't exist in our data base")
+#----------CREATING AN AGENT------------
+temp_alert=Agent(name="Temperature_Alert_Agent",seed="Alert recovery phase",endpoint=['https://api.openweathermap.org'])
 fund_agent_if_low(temp_alert.wallet.address())
 temperature_alerts = [
-    {"location": city_name, "min_temperature": min_temp, "max_temperature": max_temp},
-    
-]
+    {"location": city_name, "min_temperature": min_temp, "max_temperature": max_temp}]
 for alert in temperature_alerts:
-    @temp_alert.on_interval(period=2)  # 1800 seconds = 30 minutes
+    @temp_alert.on_interval(period=1800)  #-------1800 seconds = 30 minutes-------
     async def schedule_temperature_check(ctx: Context, alert=alert):
         await alert_temperature(ctx, alert)
-temp_alert.run()
+temp_alert.run()#-------RUNNNING AGENT-------
